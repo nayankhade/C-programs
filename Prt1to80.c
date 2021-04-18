0eb2296909a1222ae1f1da557e411d53cbbf6390
Nayan Khade <nayankhade2001@gmail.com>
2021-04-18 14:24:20 +0530

    Add Prt1to80.c

---
+++ Prt1to80.c
@@ -0,0 +1,13 @@
+/*Write a program to print number from 1 to 80 separated by tab, 8 number per line*/
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i = 1; i <= 80; i++)
+    {
+        printf("%d\t", i);
+        if (i % 8 == 0)
+            printf("\n");
+    }
+    return 0;
+}
