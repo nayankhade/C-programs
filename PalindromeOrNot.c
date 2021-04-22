07c88588d5ba2349274198cf9524b754c9a16c3d
Nayan Khade <nayankhade2001@gmail.com>
2021-04-22 14:29:07 +0530

    Add PalindromeOrNot.c

---
+++ PalindromeOrNot.c
@@ -0,0 +1,25 @@
+/* Find Whether Number Is Palindrome or Not */
+
+#include <stdio.h>
+int main()
+{
+    int n, rev = 0, rem, originalN;
+    printf("Enter a Numbers : ");
+    scanf("%d", &n);
+    originalN = n;
+    while (n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+    if (originalN == rev)
+    {
+        printf("%d is Palindrome Number", originalN);
+    }
+    else
+    {
+        printf("%d is Not Palindromw Number", originalN);
+    }
+    return 0;
+}
