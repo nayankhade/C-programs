c11bf7e762e5e3c662f14cfa0584c07d112b1eba
Nayan Khade <nayankhade2001@gmail.com>
2021-06-21 12:29:23 +0530

    Linear Search

---
+++ Data-Stracture_usingC/LinearSearch.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#define MAX 10
+int LinearSearch(int arr[], int n,int item);
+int main()
+{
+    int n,arr[MAX],item,index;
+    printf("Enter the number of elements : ");
+    scanf("%d",&n);
+    printf("Enter the elemets : \n");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the item to be searched : ");
+    scanf("%d",&item);
+    index=LinearSearch(arr, n, item);
+    if(index==-1)
+    {
+        printf("%d not found in array\n",item);
+    }
+    else
+    {
+         printf("%d found at position %d\n",item,index);
+    }
+    return 0;
+}
+int LinearSearch(int arr[], int n,int item)
+{
+    int i=0;
+    while (i<n && item!=arr[i])
+    {
+        i++;
+    }
+    if(i<n)
+    {
+        return i;
+    }
+    else
+    {
+        return -1;
+    }
+}
